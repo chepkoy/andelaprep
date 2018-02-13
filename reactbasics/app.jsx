f4d5f6f7333930f3fb7e8659429a19485ba4507e
allankiplangat <allankiplangat22@outlook.com>
2018-02-13 15:24:34 +0300

    Understanding state

--- reactbasics/app.jsx
+++ reactbasics/app.jsx
@@ -28,14 +28,20 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 var Counter = React.createClass({
-  propTypes: {
-    score: React.PropTypes.number.isRequired,
+  propTypes: {},
+
+  getInitialState: function() {
+    return {
+      score: 0,
+    }
   },
+
+
   render: function() {
     return(
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <div classNmae="counter-score">{this.props.score}</div>
+        <div classNmae="counter-score">{this.state.score}</div>
         <button className="counter-action increment"> + </button>
       </div>
     );
@@ -49,7 +55,7 @@ function Player(props){
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score = {props.score} />
+        <Counter score />
       </div>
     </div>
   );
